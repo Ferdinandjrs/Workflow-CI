dfff69bb76665c50a98f054ec6ea95543e5cc7a4
Ferdinandjrs <grc4through@gmail.com>
2026-06-08 22:06:10 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -6,13 +6,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-
-mlflow.set_experiment("Latihan Credit Scoring Lokal")
-
 def train_model():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(current_dir, 'Credit_Risk_Clean.csv')
     
+   
     if not os.path.exists(data_path):
         data_path = os.path.join(current_dir, 'namadataset_preprocessing', 'Credit_Risk_Clean.csv')
     if not os.path.exists(data_path):
@@ -29,6 +27,9 @@ def train_model():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     
+    if "MLFLOW_RUN_ID" not in os.environ:
+        mlflow.set_experiment("Latihan Credit Scoring Lokal")
+
     with mlflow.start_run(run_name="PROYEK_LOKAL_FIX_TOTAL"):
         model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
         model.fit(X_train, y_train)
@@ -36,10 +37,9 @@ def train_model():
         predictions = model.predict(X_test)
         acc = accuracy_score(y_test, predictions)
         
-        
         mlflow.log_metric("accuracy", acc)
         
-        
+        # 🎯 FIX KRITERIA 2: Memaksa simpan ke folder "model" di lokal, BEBAS DAGSHUB!
         mlflow.sklearn.log_model(sk_model=model, artifact_path="model")
         
         print(f"[+] Kriteria 2 & 3 Sukses Total! Accuracy: {acc:.4f}")
